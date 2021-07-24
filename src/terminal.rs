3a057daaa24ec33ee0d06707af8d201fb18507cb
nilpoona <snuffysmile.toolate@gmail.com>
2021-07-24 22:26:22 +0900

    implement status bar

--- src/terminal.rs
+++ src/terminal.rs
@@ -3,6 +3,7 @@ use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
+use termion::color;
 
 pub struct Size {
     pub width: u16,
@@ -19,7 +20,7 @@ impl Terminal {
         Ok(Self {
             size: Size {
                 width: size.0,
-                height: size.1,
+                height: size.1.saturating_sub(2),
             },
             _stdout: stdout().into_raw_mode()?,
         })
@@ -57,5 +58,17 @@ impl Terminal {
     }
     pub fn clear_current_line() {
         print!("{}", termion::clear::CurrentLine);
+    }
+    pub fn set_bg_color(c: color::Rgb) {
+        print!("{}", color::Bg(c));
+    }
+    pub fn reset_bg_color() {
+        print!("{}", color::Bg(color::Reset));
+    }
+    pub fn set_fg_color(c: color::Rgb) {
+        print!("{}", color::Fg(c));
+    }
+    pub fn reset_fg_color() {
+        print!("{}", color::Fg(color::Reset));
     }
 }
